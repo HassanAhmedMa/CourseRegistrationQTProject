f0fa78e09d19dc0c5e63feab4063d6b97e0ccc22
ziadEhab676 <ziadehab599@gmail.com>
2025-05-16 21:18:33 +0300

    ziad nour func

--- QtWidgetsApplication1/uploadStudentCompletedCourse.cpp
+++ QtWidgetsApplication1/uploadStudentCompletedCourse.cpp
@@ -6,20 +6,21 @@
 #include <qmessagebox.h>
 #include "QtWidgetsApplication1.h"
 #include "FilesClass.h"
+#include <string>
 using namespace std;
 
-uploadStudentCompletedCourse::uploadStudentCompletedCourse(QWidget *parent)
+uploadStudentCompletedCourse::uploadStudentCompletedCourse(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 
 	Admin* admin = QtWidgetsApplication1::currentAdmin;
-	
-	for (auto pair : FilesClass::AllCourses) {
-		string courseID = pair.first;
-		QString qCourseID = QString::fromStdString(courseID);
-		ui.courseIdComboBox->addItem(qCourseID);
-	}
+
+	connect(ui.searchBtn, &QPushButton::clicked, this, &uploadStudentCompletedCourse::on_searchBtn_clicked);
+	connect(ui.uploadStudentCompletedCourseBtn, &QPushButton::clicked, this, &uploadStudentCompletedCourse::on_uploadStudentCompletedCourseBtn_clicked);
+	connect(ui.backBtn, &QPushButton::clicked, this, &uploadStudentCompletedCourse::on_backBtn_clicked);
+
+
 }
 
 
@@ -29,15 +30,11 @@ void uploadStudentCompletedCourse::on_uploadStudentCompletedCourseBtn_clicked()
 {
 	Admin* admin = QtWidgetsApplication1::currentAdmin;
 	string studentId = ui.StudentIdLineEdit->text().toStdString();
+
 	string selectedCourseId = ui.courseIdComboBox->currentText().toStdString();
 
-	unordered_map<string, Student>& studs = FilesClass::demoStudentsMap;
-	
-	auto it = studs.find(studentId);
-	if (it == studs.end())
-		QMessageBox::information(this, "Error","Student Id not found.");
-	else
-		admin -> uploadStudentCompletedCourse(studentId, selectedCourseId, FilesClass::demoStudentsMap, FilesClass::AllCourses);
+
+	admin->uploadStudentCompletedCourse(studentId, selectedCourseId, FilesClass::demoStudentsMap, FilesClass::AllCourses);
 
 	QMessageBox::information(this, "Message", "Student's completed course uploaded successfully");
 
@@ -49,7 +46,40 @@ void uploadStudentCompletedCourse::on_backBtn_clicked()
 	this->close();
 }
 
-uploadStudentCompletedCourse::~uploadStudentCompletedCourse() 
+void uploadStudentCompletedCourse::setComboBoxItems(string studentId)
+{
+	ui.courseIdComboBox->clear();
+
+	Student& stud = FilesClass::demoStudentsMap.at(studentId);
+	vector <string> studCourses = stud.getCourses();
+	for (int i = 0; i < studCourses.size(); i++) {
+		string courseId = studCourses[i];
+		QString item = QString::fromStdString(courseId);
+		ui.courseIdComboBox->addItem(item);
+	}
+}
+
+
+
+uploadStudentCompletedCourse::~uploadStudentCompletedCourse()
 {
 
 }
+
+void uploadStudentCompletedCourse::on_searchBtn_clicked()
+{
+	string studentId = ui.StudentIdLineEdit->text().toStdString();
+	unordered_map<string, Student>& studs = FilesClass::demoStudentsMap;
+
+	auto it = studs.find(studentId);
+	if (it == studs.end()) {
+		QMessageBox::information(this, "Error", "Student Id not found.");
+	}
+	else {
+		//QMessageBox::information(this, "Success", "Student found Successfully");
+		setComboBoxItems(studentId);
+	}
+
+
+}
+
